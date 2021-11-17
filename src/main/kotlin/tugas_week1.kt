a4b08ffb6822eb88fb950f2dc825c93905140a32
ferdisetyoamanda <setyoamanda23@gmail.com>
2021-11-17 20:50:48 +0700

    ala ala

--- src/main/kotlin/tugas_week1.kt
+++ src/main/kotlin/tugas_week1.kt
@@ -1,10 +1,11 @@
 fun main() {
     val nama = "FERDI SETYO AMANDA"
-    val nim  = 20090159
+    val nim  = 200901591
     var kelas ="2C"
     val prodi = "Teknik Informatika"
     println("Nama $nama, nim $nim, kelas $kelas, prodi $prodi")
     val intro = "Nama $nama,nim $nim, kelas $kelas, prodi $prodi"
 
     println("Panjang karakternya ${intro.length} huruf")
+    hhbjhjnjnjnjk
 }
